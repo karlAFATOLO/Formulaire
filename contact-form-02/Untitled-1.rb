3b20b5afd0a50c885f79867a2532e0f6f4dfc1f9
karl AFATOLO <49211239+karlAFATOLO@users.noreply.github.com>
2021-10-26 14:38:24 +0200

    second

---
+++ contact-form-02/Untitled-1.rb
@@ -0,0 +1,51 @@
+ 
+
+
+	
+Le nom ? : Formulaire de demandes de renseignements
+	
+Est ce le même pour les prospects étudiants et entreprise ? : Non
+	
+Si non, comment faire la différence ? : Un contenu intermédiaire permettant de choisir si on souhaite afficher le formulaire de demande de renseignements pour étudiant ou pour entreprise
+	
+Quels champs pour les formulaires ? :
+	
+
+		
+Etudiant :
+		
+
+			
+Informations : Nom, prénom, adresse/
+CP/ville (non obligatoire), 
+téléphone (non obligatoire),
+email (obligatoire)
+			
+Année scolaire :
+ Nom établissement actuel,
+  adresse/CP/ville et classe 
+  actuelle
+			
+Classe Estiam demandée 
+(case à cocher) : E1, E2 et E3
+			
+Spécialisation souhaité
+ (case à cocher) :
+  CCSN/BDBI/WMD 
+  (E5 / à faire apparaitre si 
+  E3 coché)
+			
+Intéressé par le cursus métier
+ (Case à cocher / à faire apparaitre 
+ si E3 coché)
+		
+		
+		
+Entreprise :
+		
+Informations : Nom entreprise,
+Adresse/Cp/Ville entreprise,
+Nom/Prénom représentant entreprise,
+téléphone, adresse email, 
+intéressé par (case à cocher) : 
+Stagiaire, Apprenti
